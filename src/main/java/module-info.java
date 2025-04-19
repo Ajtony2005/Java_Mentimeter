faf17ddefde7b539721872ce27bfc81200e07314
Ajtony2005 <79107320+Ajtony2005@users.noreply.github.com>
2025-04-19 09:51:08 +0200

    Start the project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module hu.ppke.itk.nagyhazi {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens hu.ppke.itk.nagyhazi to javafx.fxml;
+    exports hu.ppke.itk.nagyhazi;
+}
